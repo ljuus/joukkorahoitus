8b8f6f6c933d087633f2eea4a877264177ac5430
Juuso Linja <juuso.linja@helsinki.fi>
2025-03-27 18:12:31 +0200

    register/login/logout and new item form added

---
+++ app.py
@@ -0,0 +1,78 @@
+import sqlite3
+from flask import Flask
+from flask import redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+import config
+import db
+
+app = Flask(__name__)
+app.secret_key = config.secret_key
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    description = request.form["description"]
+    target_sum = request.form["target_sum"]
+    user_id= session["user_id"]
+
+    sql = """INSERT INTO items (title, description, target_sum, user_id)
+             VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, description, target_sum, user_id])
+    
+    return redirect("/")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
+
+        if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
+            session["username"] = username
+            return redirect("/")
+        else:
+            return "VIRHE: väärä tunnus tai salasana"
+
+@app.route("/logout")
+def logout():
+    del session["user_id"]
+    del session["username"]
+    return redirect("/")
